23dbb7084ba50dbb38b951accf9dd31fce0522ed
WendyOmollo <omollow280@gmail.com>
2019-09-08 17:00:08 +0300

    Create a test that asserts if a hero has been made

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,3 +1,16 @@
-public class Hero {
+    public class Hero {
 
-}
+    private String name;
+    private int age;
+    private String strength ;
+    private String weakness;
+
+    public Hero(String name,int age,String strength,String weakness){
+        this.name = name;
+        this.age = age;
+        this.strength = strength;
+        this.weakness = weakness;
+
+        }
+
+    }
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,5 +1,6 @@
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -12,5 +13,9 @@ public class HeroTest  {
     @After
     public void tearDown() throws Exception {
     }
-    
+    @Test
+    public void makeFirstHero_confirmIfMade_true() throws Exception{
+        Hero hero = new Hero("Batman",20,"Flying","Angry");
+        assertEquals(true,hero instanceof Hero);
+    }
 }
