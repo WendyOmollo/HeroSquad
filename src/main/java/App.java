2093e152f84456de0ea0098d6e2c24c7696631d1
WendyOmollo <omollow280@gmail.com>
2019-09-09 11:13:46 +0300

    Add a form for creating a squad

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,21 +12,21 @@ import static spark.Spark.*;
     public class App {
         public static void main(String[] args) {
             staticFileLocation("/public");
-
+//displays the landing page
             get("/",(req,res)->{
             Map<String, Object> model = new HashMap<>();
                 ArrayList<Hero> heroes = Hero.getAll();
                 model.put("heroes",heroes);
                 return new ModelAndView(model,"index.hbs");
             },new HandlebarsTemplateEngine());
-
+//gets the form for adding a hero
             get("/form",(request,response) ->{
                 Map<String, Object>model = new HashMap<String,Object>();
                 return new ModelAndView(model,"hero-form.hbs");
             },new HandlebarsTemplateEngine());
 
-
-            get("/hero-card",(request,res)->{
+//gets the input from the user
+            get("/",(request,res)->{
                 Map<String, Object> model = new HashMap<>();
                 String heroName = request.queryParams("heroName");
                 String heroAge = request.queryParams("heroAge");
@@ -36,8 +36,21 @@ import static spark.Spark.*;
                 model.put("heroAge",heroAge);
                 model.put("heroStrength",heroStrength);
                 model.put("heroWeakness",heroWeakness);
-                return new ModelAndView(model,"hero-card.hbs");
+                return new ModelAndView(model,"index.hbs");
             },new HandlebarsTemplateEngine());
+
+            post("/hero-card",(req,res)->{
+                Map<String, Object> model = new HashMap<>();
+                ArrayList<Hero> heroes = Hero.getAll();
+                model.put("heroes",heroes);
+                return new ModelAndView(model,"index.hbs");
+            },new HandlebarsTemplateEngine());
+
+            get("/formSquad",(request,response) ->{
+                Map<String, Object>model = new HashMap<String,Object>();
+                return new ModelAndView(model,"squad-form.hbs");
+            },new HandlebarsTemplateEngine());
+
 
         }
     }
