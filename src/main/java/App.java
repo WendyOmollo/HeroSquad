1bb41c2bb760950def8276668fec221dc0f7fb6d
WendyOmollo <omollow280@gmail.com>
2019-09-09 09:29:52 +0300

    Add a form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,5 +19,25 @@ import static spark.Spark.*;
                 model.put("heroes",heroes);
                 return new ModelAndView(model,"index.hbs");
             },new HandlebarsTemplateEngine());
+
+            get("/form",(request,response) ->{
+                Map<String, Object>model = new HashMap<String,Object>();
+                return new ModelAndView(model,"hero-form.hbs");
+            },new HandlebarsTemplateEngine());
+
+
+//            get("/hero-card",(req,res)->{
+//                Map<String, Object> model = new HashMap<>();
+//                String heroName = request.queryParams("heroName");
+//                int heroAge = request.queryParams("heroAge");
+//                String heroStrength = request.queryParams("heroStrength");
+//                String heroWeakness = request.queryParams("heroWeakness");
+//                model.put("heroName",heroName);
+//                model.put("heroAge",heroAge);
+//                model.put("heroStrength",heroStrength);
+//                model.put("heroWeakness",heroWeakness);
+//                return new ModelAndView(model,"");
+//            },new HandlebarsTemplateEngine());
+
         }
     }
