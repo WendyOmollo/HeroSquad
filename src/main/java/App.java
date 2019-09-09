082ebb785bc88d3ae8aff27f516f3d034db4b5ee
WendyOmollo <omollow280@gmail.com>
2019-09-09 09:02:08 +0300

    Add a user interface to the project
    Use routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,23 @@
-public class App {
-}
+import models.*;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+    public class App {
+        public static void main(String[] args) {
+            staticFileLocation("/public");
+
+            get("/",(req,res)->{
+            Map<String, Object> model = new HashMap<>();
+                ArrayList<Hero> heroes = Hero.getAll();
+                model.put("heroes",heroes);
+                return new ModelAndView(model,"index.hbs");
+            },new HandlebarsTemplateEngine());
+        }
+    }
